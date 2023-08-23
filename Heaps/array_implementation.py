5897aaebcbb6f6814cfc13ca1aed17a38ff467b1
Ishan <koradiaishan335@gmail.com>
2023-08-23 18:40:33 +0530

    heapify function implementation using arrays

--- Heaps/array_implementation.py
+++ Heaps/array_implementation.py
@@ -9,12 +9,12 @@ for all nodes
 
 
 class Heap:
-    def __init__(self, level_order):
+    def __init__(self, arr: list):
         """
         Array representation of a heap is nothing but the level
         order of binary tree that represents the above properties
         """
-        self.arr: list = level_order
+        self.arr: list = Heap.heapfiy(arr)
 
     def size(self):
         return len(self.arr)
@@ -43,13 +43,58 @@ class Heap:
             else:
                 break
 
+    @classmethod
+    def heapfiy(cls, arr1: list):
+        """Transform the array into a heap with the min heap property"""
 
-level_order_heap = [2, 4, 5, 11, 6, 7, 8, 20, 12]
-hp = Heap(level_order=level_order_heap.copy())
+        # consider the arr as the level order of a complete binary tree that does not yet have a heap property
+        # all the leaf nodes already satisfy the heap property so we need from the parent of last element
+        last_idx = len(arr1) - 1
+
+        parent_last = (last_idx - 1) // 2
+
+        for i in range(parent_last, -1, -1):
+            Heap.downheapify(arr1, i)
+
+        return arr1
+
+    @classmethod
+    def downheapify(cls, heap: list, root_idx: int) -> None:
+        """Heapify the heap from ith ele to the last level with leaf nodes"""
+        n = len(heap)
+        while 2 * root_idx + 1 < n:
+            left_idx = 2 * root_idx + 1
+            right_idx = 2 * root_idx + 2
+            min_val = min([heap[root_idx], heap[left_idx], heap[right_idx]])
+
+            if min_val == heap[root_idx]:
+                break
+            elif min_val == heap[left_idx]:
+                # swap root,left
+                temp = heap[root_idx]
+                heap[root_idx] = heap[left_idx]
+                heap[left_idx] = temp
+
+                root_idx = left_idx
+            else:
+                # swap root,right
+                temp = heap[root_idx]
+                heap[root_idx] = heap[right_idx]
+                heap[right_idx] = temp
+
+                root_idx = right_idx
+
+        return None
+
+
+# you can pass any array, our class will heapify and create a valid min heap
+arr = [20, 12, 11, 7, 8, 6, 2, 4, 5]
+
+hp = Heap(arr.copy())
+assert hp.arr == [2, 4, 6, 5, 8, 20, 11, 7, 12]
 
 hp.insert(3)
 
-assert hp.size() == len(level_order_heap) + 1
+assert hp.size() == len(arr) + 1
 assert hp.arr[1] == 3
 assert hp.arr[4] == 4
-assert hp.arr[9] == 6
