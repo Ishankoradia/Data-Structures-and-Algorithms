82e76cbd3b1977c267a1772985cbb4ccd9017a5c
Ishan <koradiaishan335@gmail.com>
2023-08-23 15:33:19 +0530

    max binary subarray xor

--- Trees/TriesPrefixTree/Bits/max_xor_pair.py
+++ Trees/TriesPrefixTree/Bits/max_xor_pair.py
@@ -3,7 +3,6 @@ Given an array of N elements
 Find the max value of XOR pair i.e. A[i] ^ A[j] should be maximum
 where i, j in (0, N-1) 
 """
-from Trees.Implement.implementation import TrieBitNode
 from Trees.TriesPrefixTree.Bits.build_bit_trie import build_bit_trie
 
 
