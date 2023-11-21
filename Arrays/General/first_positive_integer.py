041269575f95a69e7f47d4f00490e7d19ae3d393
Ishan <koradiaishan335@gmail.com>
2023-11-21 09:05:50 +0530

    first missing positive integer

---
+++ Arrays/General/first_positive_integer.py
@@ -0,0 +1,22 @@
+"""
+Given an unsorted integer array nums, return the smallest missing positive integer.
+
+You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+"""
+
+
+def firstMissingPositive(self, nums) -> int:
+    N = len(nums)
+    for i in range(N):
+        j = i
+        while nums[i] > 0 and nums[i] <= N and nums[i] != nums[nums[i] - 1]:
+            # swap A[j] with A[A[j]-1]
+            temp = nums[i]
+            nums[i] = nums[temp - 1]
+            nums[temp - 1] = temp
+
+    for i in range(N):
+        if nums[i] != i + 1:
+            return i + 1
+
+    return N + 1
