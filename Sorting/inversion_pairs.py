0cb7e5dabb0e16a657fed0a4f731cbe98ea73c65
Ishan <koradiaishan335@gmail.com>
2023-11-25 13:30:11 +0530

    inversion pair count - using merge sort logic

--- Sorting/inversion_pairs.py
+++ Sorting/inversion_pairs.py
@@ -1,13 +1,13 @@
 """
-Merge sort algo
-
-TC: O(nlogn)
-SC: O(n)
+Given an array of integers A. 
+If i < j and A[i] > A[j], then the pair (i, j) is called an inversion of A. 
+Find the total number of inversions of A modulo (10**9 + 7).
 """
 
 
 def merge(arr, s, mid, e):
     temp = [0 for _ in range(e - s + 1)]
+    inv_cnt = 0
 
     p1 = s
     p2 = mid + 1
@@ -19,6 +19,7 @@ def merge(arr, s, mid, e):
         else:
             temp[p3] = arr[p2]
             p2 += 1
+            inv_cnt += mid - p1 + 1
         p3 += 1
 
     while p1 <= mid:
@@ -34,28 +35,24 @@ def merge(arr, s, mid, e):
     for i in range(s, e + 1):
         arr[i] = temp[i - s]
 
+    return inv_cnt
+
 
 def merge_sort(A, s, e):
     if s == e:
-        return
+        return 0
 
     mid = (s + e) // 2
-    merge_sort(A, s, mid)
-    merge_sort(A, mid + 1, e)
-    merge(A, s, mid, e)
+
+    inv_left = merge_sort(A, s, mid)
+    inv_right = merge_sort(A, mid + 1, e)
+    inv = merge(A, s, mid, e)
+
+    return (inv + inv_left + inv_right) % (10**9 + 7)
 
 
-def sorting_merge(A):
-    merge_sort(A, 0, len(A) - 1)
-
-    return A
+def inversion_pairs(A):
+    return merge_sort(A, 0, len(A) - 1) % (10**9 + 7)
 
 
-arr = [4, 3, 1, 10, -1]
-assert sorting_merge(arr) == sorted(arr)
-arr = [1]
-assert sorting_merge(arr) == sorted(arr)
-arr = [3, 2, 1]
-assert sorting_merge(arr) == sorted(arr)
-arr = [45, 10, 15, 25, 50]
-assert sorting_merge(arr) == sorted(arr)
+assert inversion_pairs([3, 4, 1, 2]) == 4
